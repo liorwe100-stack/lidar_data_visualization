f0cd361c14a9e4c2a15c82bb7e11d9daf5a6aa8f
liorwe100 <liorwe100@gmail.com>
2026-03-11 14:17:39 +0200

    pressing b and clicking somewhere with a mouse will add a robot circle, (next update will add a class for each robot and meybe a class for all the robots that hold each robot class in a list or something

--- visualize_lidar_pointcloud.py
+++ visualize_lidar_pointcloud.py
@@ -1,3 +1,5 @@
+from sys import float_repr_style
+
 import pygame
 import csv
 import sys
@@ -16,19 +18,25 @@ SCALE = 6        # scale lidar units → pixels
 OFFSET_X = 40    # shift drawing right
 OFFSET_Y = 40    # shift drawing down
 MAX_DIST = 1.4     # lidar units
-CAMERA_SPEED = 10
+CAMERA_SPEED = 30
 
 # ====================
 # Load LiDAR point cloud
 # ====================
-points = []
-
+lidar_points = []
+robot_points = []
 with open("lidar_apartment_2d_pointcloud.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         x = float(row["x"])
         y = float(row["y"])
-        points.append((x, y))
+        lidar_points.append((x, y))
+with open("robot_locations.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        x = float(row["x"])
+        y = float(row["y"])
+        robot_points.append((x, y))
 
 # ====================
 # Pygame setup
@@ -42,18 +50,31 @@ clock = pygame.time.Clock()
 # Main loop
 # ====================
 running = True
+b_pressed = False
 while running:
     clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #left click
+            if b_pressed:
+                mx,my = event.pos
+                x = (mx - OFFSET_X) / SCALE
+                y = (my - OFFSET_Y) / SCALE
+                new_robot = (x,y)
+                with open("robot_locations.csv", "a", newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(new_robot)
+                robot_points.append(new_robot)
+                b_pressed = not b_pressed
         if event.type == pygame.MOUSEWHEEL:
             SCALE = max(1, SCALE + event.y)
         if event.type == pygame.KEYDOWN:
             keys = pygame.key.get_pressed()
             speed = CAMERA_SPEED * 3 if keys[pygame.K_LSHIFT] else CAMERA_SPEED
-
+            if keys[pygame.K_b]:
+                b_pressed = not b_pressed
             if keys[pygame.K_a] or keys[pygame.K_LEFT]:
                 OFFSET_X += speed
             if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
@@ -64,23 +85,26 @@ while running:
                 OFFSET_Y -= speed
 
     screen.fill(BACKGROUND_COLOR)
+    for x, y in robot_points:
+        px = int(x * SCALE + OFFSET_X)
+        py = int(y * SCALE + OFFSET_Y)
+        pygame.draw.circle(screen, POINT_COLOR, (px, py), POINT_RADIUS + 10)
 
-    for x, y in points:
+    for x, y in lidar_points:
         px = int(x * SCALE + OFFSET_X)
         py = int(y * SCALE + OFFSET_Y)
         pygame.draw.circle(screen, POINT_COLOR, (px, py), POINT_RADIUS)
 
-
-    for i in range(len(points)):
-        for j in range (i, len(points)):
-            x1, y1 = points[i]
-            x2, y2 = points[j]
+    for i in range(len(lidar_points) - 1):
+        for j in range (i + 1, len(lidar_points)):
+            x1, y1 = lidar_points[i]
+            x2, y2 = lidar_points[j]
             dist = math.hypot(x2 - x1, y2 - y1)
             if dist < MAX_DIST:
                 p1 = (int(x1 * SCALE + OFFSET_X), int(y1 * SCALE + OFFSET_Y))
                 p2 = (int(x2 * SCALE + OFFSET_X), int(y2 * SCALE + OFFSET_Y))
                 pygame.draw.line(screen, (255,0,255), p1, p2, 1)
-    # points.sort(
+    # lidar_points.sort(
     #     key=lambda p: math.atan2(p[1] - LIDAR_Y, p[0] - LIDAR_X)
     # )
     pygame.display.flip()
