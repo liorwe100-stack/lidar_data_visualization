6b93b991cacb58b17fc518929ba6fb077cd1a8a4
liorwe100 <liorwe100@gmail.com>
2026-03-13 00:11:59 +0200

    fixed robot creating offset misaligning

--- robot_node.py
+++ robot_node.py
@@ -4,8 +4,8 @@ class Robot_node:
         self.robot_pos = robot_pos
         self.x = self.robot_pos[0]
         self.y = self.robot_pos[1]
-        self.new_x = (self.x + offset_x) / scale
-        self.new_y = (self.y + offset_y) / scale
+        self.new_x = (self.x - offset_x) / scale
+        self.new_y = (self.y - offset_y) / scale
         self.new_robot_pos = (self.new_x, self.new_y)
         with open("robot_locations.csv", "a", newline='') as file:
             writer = csv.writer(file)
